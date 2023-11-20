aa84489f4200ec0be7c2ab10d26702176b49fd2a
Noureddine145 <133378203+Noureddine145@users.noreply.github.com>
2023-11-20 14:41:44 +0100

    Add files via upload

--- conectie roepen.php
+++ conectie roepen.php
@@ -1,4 +1,11 @@
 <?php
 include ("databese-conectie.php");
 $database = new Database();
+
+try {
+    $database->insertUser("ensar", "ensar");
+    echo "Succes";
+} catch (PDOException $e) {
+    echo "Error inserting' . ".$e->getmessage();
+}
 ?>
--- databese-conectie.php
+++ databese-conectie.php
@@ -3,7 +3,7 @@
 class Database {
     public $pdo;
 
-    public function __construct($db = "winkel" , $user="root" , $pass="", $host="localhost") {
+    public function __construct($db = "winkel" , $user="root" , $pass="", $host="localhost:3307") {
         try {
             $this->pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,6 +11,11 @@ class Database {
         } catch(PDOExceprion $e) {
             echo "Error: " . $e->getMessage();
         }
+    }
+    public function insertUser($a, $b) {
+        $sql = "INSERT INTO account VALUES (null, :gebruikersnaam, :password)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute([':gebruikersnaam' => $a, ':password' => $b]);
     }
 }
 
